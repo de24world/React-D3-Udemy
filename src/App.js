4bf30b525a351f03ab613d1eca9df3a79e3b2468
HoSoo <hosoo.lee@arithnea.de>
2020-06-22 01:47:24 +0200

    8.React recap*

--- src/App.js
+++ src/App.js
@@ -2,12 +2,21 @@ import React, { Component } from "react";
 import Test from "./Test";
 
 export default class App extends Component {
+  state = {
+    personClicks: 0,
+  };
+
+  personClicked = () => {
+    this.setState({ personClicks: this.state.personClicks + 1 });
+  };
+
   render() {
     return (
       <div className="App">
-        <Test person="A" />
-        <Test person="B" />
-        <Test person="C" />
+        <Test person="A" personClicked={this.personClicked} />
+        <Test person="B" personClicked={this.personClicked} />
+        <Test person="C" personClicked={this.personClicked} />
+        Person Clicks: {this.state.personClicks}
       </div>
     );
   }
--- src/Test.js
+++ src/Test.js
@@ -1,5 +1,5 @@
 import React from "react";
 
 export default function Test(props) {
-  return <div>Hello {props.person}</div>;
+  return <div onClick={props.personClicked}>Hello {props.person}</div>;
 }
