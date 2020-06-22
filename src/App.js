3d8d7fe25f08a2e0ed217a67c4c7ea4ba6c2b638
HoSoo <hosoo.lee@arithnea.de>
2020-06-22 23:10:48 +0200

    10. Creating our React wrapper

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
 import React, { Component } from "react";
-import Test from "./Test";
+import ChartWrapper from "./ChartWrapper";
 
 export default class App extends Component {
-  state = {
-    personClicks: 0,
-  };
-
-  personClicked = () => {
-    this.setState({ personClicks: this.state.personClicks + 1 });
-  };
-
   render() {
     return (
       <div className="App">
-        <Test person="A" personClicked={this.personClicked} />
-        <Test person="B" personClicked={this.personClicked} />
-        <Test person="C" personClicked={this.personClicked} />
-        Person Clicks: {this.state.personClicks}
+        <ChartWrapper />
       </div>
     );
   }
