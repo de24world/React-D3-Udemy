67a1655ac7dc1b3481b69ae0feb5c2ab0480bf2f
HoSoo <hosoo.lee@arithnea.de>
2020-06-28 23:59:08 +0200

    18.setting up our project*

--- src/ChartWrapper.js
+++ src/ChartWrapper.js
@@ -2,11 +2,12 @@ import React, { Component } from "react";
 import D3Chart from "./D3Chart";
 
 export default class ChartWrapper extends Component {
-  componentDidMount() {
-    new D3Chart(this.refs.chart);
-  }
+  chartRef = React.createRef();
 
+  componentDidMount() {
+    new D3Chart(this.chartRef.current);
+  }
   render() {
-    return <div ref="chart"></div>;
+    return <div ref={this.chartRef}></div>;
   }
 }
--- src/D3Chart.js
+++ src/D3Chart.js
@@ -1,34 +1,26 @@
 import * as d3 from "d3";
 
-// const data = [20, 12, 16, 25, 20];
-const url = "https://udemy-react-d3.firebaseio.com/ages.json";
+const url = "https://udemy-react-d3.firebaseio.com/tallest_men.json";
 
 export default class D3Chart {
   constructor(element) {
     const svg = d3
       .select(element)
       .append("svg")
-      .attr("width", 500)
+      .attr("width", 800)
       .attr("height", 500);
 
-    d3.json(url).then((agesData) => {
-      const rects = svg.selectAll("rect").data(agesData);
-      // console.log(agesData); check data
+    d3.json(url).then((data) => {
+      const rects = svg.selectAll("rect").data(data);
 
       rects
         .enter()
         .append("rect")
         .attr("x", (d, i) => i * 100)
-        .attr("y", 50)
+        .attr("y", 0)
         .attr("width", 50)
-        .attr("height", (d) => d.age * 10)
-        .attr("fill", (d) => {
-          if (d.age > 10) {
-            return "red";
-          } else {
-            return "green";
-          }
-        });
+        .attr("height", (d) => d.height)
+        .attr("fill", "grey");
     });
   }
 }
